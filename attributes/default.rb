44dc5c9c4b2c83733828ed26add92a38c2b49ed1
Mike Taylor <mike.taylor@shopify.com>
2017-10-17 16:29:03 -0700

    bug fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ include_attribute 'deploy'
 default[:sidekiq] = {}
 
 node[:deploy].each do |application, deploy|
-  puts "DEBUG DEFAULT #{worker.inspect}"
+  puts "DEBUG DEFAULT #{application.inspect}"
   puts "DEBUG DEFAULT #{deploy.inspect}"
   default[:sidekiq][application.intern] = {}
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart sidekiq_swarm"
